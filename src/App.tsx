529d47b4f102bf25025d3f5825a49f6b2ed20148
trungvv <trungvv@vtvlive.vn>
2025-05-09 16:47:01 +0700

    add route

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import './App.scss'
 
 function App() {
   return (
-    <div className='md:hidden text-primary flex-center'>
+    <div className='text-primary'>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima amet maxime quasi, nulla dignissimos rerum expedita! Commodi dolorem ut magnam dolores perspiciatis alias iure! Ipsum deleniti eligendi aliquid architecto alias.
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router"
 import App from './App.tsx'
+import Landing from './page/Landing.tsx'
 import ScaleLayout from './components/ScaleLayout.tsx'
+import './index.css'
+import './index.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ScaleLayout>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="landing" element={<Landing />} />
+        </Routes>
+      </BrowserRouter>
     </ScaleLayout>
   </StrictMode>,
 )
